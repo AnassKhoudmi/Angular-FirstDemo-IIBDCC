c389bfbefbb5ea7c91dd99f866d4e83e762423fc
anasskhoudmi <a_khoudmi@etu.enset-media.ac.ma>
2024-05-13 04:11:00 +0100

    Version Finale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   title = 'FirstApp1';
+
+  public currentRoute : any;
+
+  constructor(private router : Router){
+  }
+
+  gotoHome() {
+    this.currentRoute = "home";
+    this.router.navigateByUrl("/home");
+  }
+
+  gotoproducts() {
+    this.currentRoute = "products";
+    this.router.navigateByUrl("/products");
+  }
 }
